21259ca7c2c06417a2b6567a582d32f8ed11a671
maitanishqhoon <ramchandanitanishq@gmail.com>
2024-08-14 20:26:02 +0530

    Learning more about react library by trial and error

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
 import Chai from "./chai";
 function App() {
+  const username = 'chai aur code'
   return (
+    <>
     <Chai />
+    <h1>{username}</h1>
+    </>
   );
 }
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App/>
 )
